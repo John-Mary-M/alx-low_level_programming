d8447167cb867bb038ff18394c941daf8d4c6d6c
John-Mary-M <johnmaryluxin8@gmail.com>
2023-03-16 07:54:20 -0700

    fixxed bug in 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@ int main(void)
 {
 
 	printf("The size of an int is: %d bytes.\n", sizeof(int));
-	printf("The size of a double is: %lf bytes.\n", sizeof(double));
 	printf("The size of a c is: %d.\n", sizeof(char));
 	printf("The size of a long int: %d bytes\n", sizeof(long int));
 	printf("The size of a float: %d byte(s)\n", sizeof(float));
