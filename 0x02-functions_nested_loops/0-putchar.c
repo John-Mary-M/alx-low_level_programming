9ee63f504c12c324aebfc6f7135470852a35a303
John-Mary-M <johnmaryluxin8@gmail.com>
2023-03-21 03:41:19 -0700

    created main.h, fixed forward declaration in 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-#include "_putchar.c"
+#include "main.h"
 
 /**
  * main - Entry point
  *
  * _putchar - writes the string _putchar to stdout
+ * @c: The character to print.
  *
  * Return: On success 0.
  * On error, -1 is returned.
  */
+int _putchar(char c);
 int main(void)
 {
-	char *str = "_putchar\n";
+	int *str = "_putchar\n";
 	int i = 0;
 
 	/* string represented as an array of chars terminated by null char '\0'*/
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "_putchar.c"
+
+/**
+ * main - Entry point.
+ *
+ * _putchar - writes alphabet in lowercase to stdout
+ *
+ * Return: On success 0.
+ * On error, -1 is returned.
+ */
+int main(void)
+{
+	int *i;
+
+	for (i == 'a'; i <= 'z'; i++)
+	{
+		_putchar(char *);
+	}
+	_putchar("\n");
+	return (0);
+}
