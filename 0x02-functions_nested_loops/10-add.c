e35ed109f3694958eddf12081b716e809a9cf5cb
John-Mary-M <johnmaryluxin8@gmail.com>
2023-03-21 15:05:50 -0700

    wrote 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *add - adds two integers
+ *@int: is the int type of the integer
+ * Return: Always 0.
+ */
+int add(int x, int y)
+{
+	/*int x;*/
+	/*int y;*/
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
