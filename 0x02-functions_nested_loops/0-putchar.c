a4e58fc77fe6eb26f6a3cb2db820972e1f54a8da
John-Mary-M <johnmaryluxin8@gmail.com>
2023-03-21 07:26:29 -0700

    0-putchar.c finally complete

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
-#include <stdio.h>
 #include "main.h"
+
 
 /**
  * main - Entry point
  *
  * _putchar - writes the string _putchar to stdout
- * @c: The character to print.
  *
  * Return: On success 0.
  * On error, -1 is returned.
  */
-int _putchar(char c);
 int main(void)
 {
-	int *str = "_putchar\n";
+	char *str = "_putchar\n";
 	int i = 0;
 
-	/* string represented as an array of chars terminated by null char '\0'*/
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
@@ -24,5 +21,4 @@ int main(void)
 	}
 
 	return (0);
-
 }
