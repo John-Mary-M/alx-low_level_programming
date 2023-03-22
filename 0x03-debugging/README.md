f79e855bf9ba32ab22df5adc274792e8c97b402e
John-Mary-M <johnmaryluxin8@gmail.com>
2023-03-22 02:22:46 -0700

    Debugging task 0 solved

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+In this project we learn to debug, and how to read and fix errors based on error
+messages and warnings.
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * positive_or_negative - Checks for positive or negative.
+ *@i: Number to check.
+ *
+ *Return: Always 0 (Success)
+ */
+void positive_or_negative(int n)
+{
+
+	srand(time(0));
+	/*n = rand() - RAND_MAX / 2;*/
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return;
+}
