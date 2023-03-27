5b6c648e29693cacad4b4f9394e036eb2e00c755
John-Mary-M <johnmaryluxin8@gmail.com>
2023-03-27 03:07:27 -0700

    wrote 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of input to 98
+ * @n: address of variable before update
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	int *f;
+
+	f = n;
+	*f = 98; /* updating value */
+	return;
+}
