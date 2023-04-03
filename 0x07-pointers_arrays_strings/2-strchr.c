16a44ab7cfea58275caa412fa75038b573f962ca
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-03 10:10:55 -0700

    fixed bugs in 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be searched
+ * @c: character to search for in s.
+ * Return: pointer to c if c is found in s
+ * null if c is not found in s. null is the s null byte \0.
+ */
+char *_strchr(char *s, char c)
+{
+	/*search for c in s, search till '\0' */
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	/* return pointer to null s element if c not found*/
+	return (s);
+}
