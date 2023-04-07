75b6acd5fb895e5bc809e5bfb8ceb27e1135bed8
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-07 05:35:30 -0700

    wrote 0-whatsmyname.c using standard library

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - checks code
+ *@argc: number of arguments passed in
+ *@argv: pointer to the character array with all strings passed
+ * Return: 0 on success.
+ */
+int main(int argc, char **argv)
+{
+	/*name(argv);*/
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
