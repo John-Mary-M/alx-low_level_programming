419b1a8a21780fdfe5ae7bdce0fb814d3ba3e5b3
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-13 06:33:43 -0700

    wrote 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: number of bytes to reserve
+ * Return: pointer to the allocated memory else terminate process with status
+ * 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ret_ptr;
+
+	ret_ptr = malloc(b);
+	if (ret_ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ret_ptr);
+}
