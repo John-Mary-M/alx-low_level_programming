38c718e22a7dc6e7345fea5e9209347188da52ee
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-17 11:45:59 -0700

    wrote 2-print_dog.c sticking with the -> method

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,6 +16,9 @@ void print_dog(struct dog *d)
 	/* check if *d is NULL*/
 	if (d == NULL)
 		printf(" ");
+	else
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age,
+		       d->owner);
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	/*printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);*/
 }
