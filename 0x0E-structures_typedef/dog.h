aa71a9c2efc7580aac31000eec38f917353f2c0b
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-17 10:54:18 -0700

    wrote dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - brief description
+ * @name: first member
+ * @age: second member
+ * @owner: thrid member
+ * populates variables of struct dog type.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* dog_h */
