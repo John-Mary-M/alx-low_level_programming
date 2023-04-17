84d77688b147f0c918dcae4d29f1e88d1561b0a2
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-17 13:49:48 -0700

    wrote 5-free_dog.c used free func from stdlib.h

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free space occupied by dog objects
+ * @d: pointer to the dog object to be freed
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	/*using free() to free previously assigned space*/
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
