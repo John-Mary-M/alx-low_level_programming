902409dce25c145d704ec5117728a8ecc0745c76
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-19 13:16:07 -0700

    bug in 3-get_op_func.c fixed exit 99 inserted

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].f != NULL)
 	{
 		if (*ops[i].op == *s && *(s + 1) == '\0')
 			return (ops[i].f);
 		i++;
 	}
-	return (NULL);
+        printf("Error\n");
+	exit(99);
 }
