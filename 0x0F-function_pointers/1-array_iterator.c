588668a6542033e9e2f8bef77aef2cb46a4e6c86
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-19 06:08:51 -0700

    used recursion to iterate through an array of pointers to function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of
+ * an array
+ * @array: an int array with pointer to functions
+ * @size: constant determining the size of the array
+ * @action: pointer to function with int parameter.
+ * Return: void.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+        if (size == 0)/*Recursion base case*/
+	{
+		return;
+	}
+
+	/* action called with the 0th element of the arry*/
+	action(*array);
+	/*recursive call to array_itarator*/
+	array_iterator(array + 1, size - 1, action);
+}
