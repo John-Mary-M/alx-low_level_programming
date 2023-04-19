cc6dce78eddb9a58098479d3fe4e8c343626fa85
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-19 03:44:02 -0700

    directory created and wrote 0-print_name, function_pointers.h, and  README.md

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - prints a name.
+ * @name: name to print.
+ * @f: function pointer with name as a parameter.
+ * Return: Void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)/*check if either arguements is NULL*/
+		exit(0);
+
+	while (*name != '\0')/*iterate over each character in name*/
+	       f(*name++);/**
+			    *call print_name by use of function pointer f with
+			    * each character as an argument
+			    */
+
+	f('\n');/* call print_name with newline character as an argument*/
+}
