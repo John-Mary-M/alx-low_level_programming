ff12c56e9711014a918709e1aae112ba381ea94c
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-20 08:43:18 -0700

    updated variadic_function.h header file and changed the implementation in 3-print_all.c function.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,53 +4,89 @@
 #include "variadic_functions.h"
 
 /**
+ * print_c - prints char
+ * @a: list
+ * Return: always zero.
+ */
+int print_c(va_list a)
+{
+	printf("%c", va_arg(a, int));
+	return (0);
+}
+/**
+ * print_i - prints int
+ * @a: int to give
+ * Return: always 0.
+ */
+int print_i(va_list a)
+{
+	printf("%d", va_arg(a, int));
+	return (0);
+}
+/**
+ * print_f - prints float
+ * @a: list to give
+ * Return: always 0
+ */
+int print_f(va_list a)
+{
+	printf("%f", va_arg(a, double));
+	return (0);
+}
+/**
+ * print_s - prints string
+ * @a: list to give
+ * Return: always 0.
+ */
+int print_s(va_list a)
+{
+	char *s;
+
+	s = va_arg(a, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return (0);
+	}
+	printf("%s", s);
+	return (0);
+}
+/**
  * print_all - prints anything
  * @format:  list of types of arguments passed to the function
  * Return: void
  */
 void print_all(const char * const format, ...)
 {
+	int i, j;
+	char *sep = "";
+	char *sep2 = ", ";
 	va_list args;
-	char c;
-	int i;
-	float f;
-	char *s;
-	i = 0;
-
-
+	printer ops[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"s", print_s},
+		{"f", print_f},
+		{NULL, NULL}
+	};
 
 	va_start(args, format);
+	i = 0;
 
-
-	while (format && format[i])
+	while (format != NULL && format[i])
 	{
-		switch (format[i])
+		j = 0;
+		while (ops[j].f != NULL)
 		{
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'c':
-				c = (char) va_arg(args, int);
-				printf("%c", c);
-				break;
-			case 's':
-				s = va_arg(args, char*);
-				if (s)
-					printf("%s", s);
-				else
-					printf("(nil)");
-				break;
-			case 'f':
-				f = (float) va_arg(args, double);
-				printf("%f", f);
-				break;
-			default:
-				break;
+			if (format[i] == *(ops[j].c))
+			{
+				printf("%s", sep);
+				ops[j].f(args);
+			}
+			j++;
 		}
-		if (format[i + 1])
-			printf(", ");
+		sep = sep2;
 		i++;
-
 	}
 	printf("\n");
 	va_end(args);
