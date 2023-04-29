3695b270abace409e0fae410fcc87f2aa96f0780
John-Mary-M <johnmaryluxin8@gmail.com>
2023-04-29 08:47:10 -0700

    fixed bug in 2-add_nodeint.c method 2 of inserting node at the begining of the list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a listint_t list.
- * @h: pointer to pointer to the first node in the list
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: pointer to pointer to the first node in the list
  * @n: new content to be added at the start of the list
  * Return: address of the new element, or NULL if it failed.
  */
@@ -10,7 +10,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t));
 
-        if (new == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
