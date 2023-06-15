96acda589b1cdab33525692b1928618058b04d8c
John-Mary-M <johnmaryluxin8@gmail.com>
2023-06-15 14:06:26 +0000

    Lines in function reduced to meet requirements

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,70 +1,52 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position in a doubly linked list
+ * insert_dnodeint_at_index - puts new node at a given position in dlinked list
  * @h: pointer to a pointer to the head of the list
  * @idx: index where the new node should be inserted
  * @n: value to be stored in the new node
- *
  * Return: pointer to the new node on success, NULL on failure
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	/* create two pointers of type dlistint_t */
 	dlistint_t *tmp, *new;
-	/* create an unsigned int to track current index */
 	unsigned int dex = 1;
 
-	/* allocate memory for new node */
-	new = malloc(sizeof(dlistint_t));
+	tmp = *h;
+
+	new = malloc(sizeof(dlistint_t));  /*allocate mem fr new node */
 	if (new == NULL)
 		return (NULL);
-	/* set value for new node */
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
-
-	/* if list is empty */
+	new->n = n;  /* set value for new node */
+	new->prev = NULL, new->next = NULL;
 	if ((*h) == NULL)
 	{
-		/* if index is 0, insert new node at beginning */
-		if (idx == 0)
+		if (idx == 0) /*if index = 0 insrt new node at bgining*/
 		{
 			*h = new;
 			return (new);
 		}
-		/* if index is not 0, return NULL */
 		return (NULL);
 	}
-
-	/* if index is 0, insert new node at beginning */
-	if (idx == 0)
+	if (idx == 0)  /* if index is 0, insrt new node at beginning */
 	{
 		new->next = *h;
 		(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
-
-	/* traverse list until end or index is found */
-	tmp = *h;
 	while (tmp->next != NULL && dex != idx)
 	{
 		tmp = tmp->next;
 		dex++;
 	}
-
-	/* if index is found, insert new node */
-	if (dex == idx)
+	if (dex == idx)  /* if index is found, insert new node */
 	{
-		new->prev = tmp;
-		new->next = tmp->next;
+		new->prev = tmp, new->next = tmp->next;
 		if (tmp->next != NULL)
 			tmp->next->prev = new;
 		tmp->next = new;
 		return (new);
 	}
-
-	/* if index is not found, return NULL */
-	return (NULL);
+	return (NULL);/* if index is not found, return NULL */
 }
